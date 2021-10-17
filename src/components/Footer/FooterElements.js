4ed845cd1b0494d01ebb7e718b5501ed26805646
nishanth <nishanth.c@dheeyantra.com>
2021-10-17 16:43:13 +0530

    Format: formated code

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -8,6 +8,12 @@ export const FooterContainer = styled.div`
   background: #000;
 `;
 
+export const FooterHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+`;
+
 export const FooterWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -21,21 +27,19 @@ export const FooterCard = styled.div`
   line-height: 2;
   width: 300px;
 `;
+
 export const FooterImg = styled.img`
   height: 300px;
   min-width: 300px;
   max-width: 100%;
   box-shadow: 4px 5px #fec500;
 `;
-export const FooterHeading = styled.h1`
-  font-size: clamp(2rem, 2.5vw, 3rem);
-  text-align: center;
-  margin-bottom: 5rem;
-`;
+
 export const FooterTitle = styled.h2`
   font-size: 1.5rem;
   font-weight: 400;
 `;
+
 export const FooterInfo = styled.div`
   display: flex;
   flex-direction: column;
@@ -44,13 +48,16 @@ export const FooterInfo = styled.div`
   padding: 2rem;
   text-align: center;
 `;
+
 export const FooterDescription = styled.p`
   margin-bottom: 1rem;
 `;
+
 export const FooterPrice = styled.p`
   margin-bottom: 1rem;
   font-size: 2rem;
 `;
+
 export const FooterBtn = styled.button`
   font-size: 1rem;
   padding: 1rem 4rem;
